0cfa3990e073ce810cd0857abe304a487de85b6b
Edi Prasetyi <ediprasetiyo2@gmail.com>
2023-07-13 11:32:50 +0700

    Update Fungsi Edit Produk

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $products = Product::orderBy('id', 'asc')->paginate(1);
+        $products = Product::orderBy('id', 'desc')->paginate(5);
         return view('admin.products.index', compact('products'));
     }
     public function create()
